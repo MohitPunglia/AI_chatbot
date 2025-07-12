d0843263e4a8ddbe5cc2ea9aa8a7e2fdf43b6c92
Mohit <mohit.punglia@gmail.com>
2025-07-12 14:13:23 +0530

    Added Partial code

--- partial_code.py
+++ partial_code.py
@@ -88,3 +88,27 @@ query = st.text_area(
 )
 
 API_URL = "http://127.0.0.1:9999/chat"
+
+if st.button("Get Response"):
+    if query.strip():
+        with st.spinner("Getting response..."):
+            payload = {
+                "model_name": model_name,
+                "messages": [query],
+                "allow_search": allow_search,
+                "system_prompt": system_prompt,
+                "model_provider": provider,
+            }
+            response = requests.post(API_URL, json=payload)
+            # response = (
+            #     "Testing response from AI Agent"  # Placeholder for actual API call
+            # )
+            # st.subheader("Response from AI Agent:")
+            # st.markdown(response)
+            if response.status_code == 200:
+                ai_response = response.json().get("response", "No response from AI.")
+                st.success(ai_response)
+            else:
+                st.error(f"Error: {response.status_code} - {response.text}")
+    else:
+        st.warning("Please enter a query.")
